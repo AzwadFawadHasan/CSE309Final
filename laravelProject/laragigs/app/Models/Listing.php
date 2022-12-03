8c6ee7b3930685b382c2ffadb35ee8d434ea371a
azwad <azwadfawadhasan@gmail.com>
2022-12-03 20:44:19 +0600

    created an elequent model

--- laravelProject/laragigs/app/Models/Listing.php
+++ laravelProject/laragigs/app/Models/Listing.php
@@ -1,34 +1,11 @@
 <?php
 
 namespace App\Models;
-class Listing{
-    public static function all(){
-        return [
-            [
-                'id'=>1,
-                'title'=>'Listing One',
-                'description'=> 'lorem epsum'
-            ],
-            [
-                'id'=>2,
-                'title'=>'Listing Two',
-                'description'=> 'lorem epsum'
-            ]
-            ];
-    }
 
-    public static function find($id){
-        $listings = self::all();  //when u have a class and you want to call another method use SELF to call a static funtion/method
-        foreach($listings  as $listing){
-            if($listing['id'] == $id){
-                return $listing;
-            }
-        }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-
-    }
+class Listing extends Model
+{
+    use HasFactory;
 }
-
-
-
-?>
