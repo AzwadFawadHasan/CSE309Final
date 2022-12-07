582af356871ae8a6889a50148d2bce7b5ab4173a
azwad <azwadfawadhasan@gmail.com>
2022-12-07 15:53:02 +0600

    blade component setup

--- laravelProject/laragigs/resources/views/components/listing-card.blade.php
+++ laravelProject/laragigs/resources/views/components/listing-card.blade.php
@@ -16,8 +16,8 @@
 
 
 
-<div class="bg-gray-50 border border-gray-200 rounded p-6">
-    <div class="flex">
+<x-card>
+        <div class="flex">
         <img
             class="hidden w-48 mr-6 md:block"
             src="{{asset('images/no-image.png')}}"
@@ -54,5 +54,5 @@
                 <i class="fa-solid fa-location-dot"></i> {{$listing->location}}
             </div>
         </div>
-    </div>
-</div>
+    </div> 
+</x-card>
