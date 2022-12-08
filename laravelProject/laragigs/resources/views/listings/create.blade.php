4582f42ee2f48324c0a81286cfc7cc7463cdb744
azwad <azwadfawadhasan@gmail.com>
2022-12-09 00:28:29 +0600

    added search route and search method

--- laravelProject/laragigs/resources/views/listings/create.blade.php
+++ laravelProject/laragigs/resources/views/listings/create.blade.php
@@ -8,7 +8,7 @@
                         <p class="mb-4">Post a gig to find a developer</p>
                     </header>
 
-                    <form method="POST" action="listings">
+                    <form method="POST" action="/listings">
                         @csrf
                         <div class="mb-6">
                             <label
